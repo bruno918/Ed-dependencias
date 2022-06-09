20e96ca71fe52653f05f6412f15b0fa64a2b6431
bruno918 <82423722+bruno918@users.noreply.github.com>
2022-06-09 08:11:43 -0300

    Create comandos termux.js

---
+++ comandos termux.js
@@ -0,0 +1,28 @@
+termux-setup-storage 
+apt update
+
+apt upgrade
+
+pkg update && pkg upgrade
+
+pkg install bash
+
+pkg install libwebp
+
+pkg install git -y
+
+pkg install nodejs -y 
+
+pkg install ffmpeg -y 
+
+pkg install wget
+
+pkg install imagemagick -y
+
+git clone https://github.com/bruno918/satorumd.git
+
+cd satorumd
+
+git clone https://github.com/bruno918/node_modules.git
+
+sh gojo.sh
